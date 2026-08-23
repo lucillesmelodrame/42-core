dce7e205842ae1fb75ba4eaf68ac1ee12b9216c2
melodrame <254338995+lucillesmelodrame@users.noreply.github.com>
2026-08-23 15:01:57 +0800

    added lstnew

---
+++ libft/ft_lstnew.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sonfong <sonfong@student.42kl.edu.my>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/23 00:01:15 by sonfong           #+#    #+#             */
+/*   Updated: 2026/08/23 02:16:28 by melodrame        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	struct s_list	*head;
+
+	head = (struct s_list *)malloc(sizeof(struct s_list));
+	if (!head)
+		return (NULL);
+	head->content = content;
+	head->next = NULL;
+	return (head);
+}
