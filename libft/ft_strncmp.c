5e8a3f180238a4d34298ae587835f8fcfd98f1dd
melodrame <254338995+lucillesmelodrame@users.noreply.github.com>
2026-08-30 17:48:30 +0800

    alluneedisloveeeeeee

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: sonfong <sonfong@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/27 15:41:02 by sonfong           #+#    #+#             */
-/*   Updated: 2026/08/09 04:46:37 by melodrame        ###   ########.fr       */
+/*   Updated: 2026/08/30 17:14:34 by melodrame        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,11 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t	i;
 
 	i = 0;
-	if (n == 0)
+	while (i < n && s1[i] && s1[i] == s2[i])
+		i++;
+	if (i == n)
 		return (0);
-	if (i < n && s1[i] && s1[i] == s2[i])
-	{
-		while (s1[i] == s2[i])
-			i++;
-	}
-	return (s1[i] - s2[i]);
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
 
 /*
